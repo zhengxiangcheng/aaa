0b7921c47fe8c92682265e267e2f6a4018abce69
zhengxiangcheng <1524872052@qq.com>
2020-01-06 12:02:45 +0800

    第二次

--- demo/src/main/java/com/yzit/test.java
+++ demo/src/main/java/com/yzit/test.java
@@ -3,5 +3,7 @@ package com.yzit;
 public class test {
     public static void main(String[] args) {
         System.out.println("aaaa");
+        System.out.println("aaaa");
+        System.out.println("aaaa");
     }
 }
